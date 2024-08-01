5baa9b999c5ac6ea594f7d5a94d6876f362461fb
Fasilfaz <fazsby2001@gmail.com>
2024-08-01 20:23:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let list = document.querySelectorAll('.list .item');
+let main = document.querySelector('.main');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let mockup = document.querySelector('.mockup');
+
+let count = list.length;
+let active = 0;
+let leftMockup = 0;
+let left_each_item = 100 / (list.length - 1);
+
+next.onclick = () => {
+    active = active >= count - 1 ? 0 : active + 1;
+    leftMockup = leftMockup + left_each_item;
+    main.classList.remove('right');
+    changeCarousel();
+}
+
+prev.onclick = () => {
+    active = active <= 0 ? count - 1 : active - 1;
+    leftMockup = leftMockup - left_each_item;
+    main.classList.add('right');
+    changeCarousel();
+}
+
+function changeCarousel() {
+    let hidden_old = document.querySelector('.item.hidden');
+    if(hidden_old) hidden_old.classList.remove('hidden');
+    let active_old = document.querySelector('.item.active');
+    active_old.classList.remove('active');
+    active_old.classList.add('hidden');
+    list[active].classList.add('active');
+    mockup.style.setProperty('--left',leftMockup+'%');
+    clearInterval(refreshTimer);
+    refreshTimer = setInterval(() => next.click(),5000);
+}
+let refreshTimer = setInterval(() => next.click(),5000)
